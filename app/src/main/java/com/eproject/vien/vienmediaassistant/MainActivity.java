a2cddab1cc0c8e26c738d0963c2cbd9ba8925f53
SandyWangWei <453915719@qq.com>
2018-07-22 19:00:50 +0800

    new project

---
+++ app/src/main/java/com/eproject/vien/vienmediaassistant/MainActivity.java
@@ -0,0 +1,13 @@
+package com.eproject.vien.vienmediaassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
